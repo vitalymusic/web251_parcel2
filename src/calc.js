3c449d7d4e56968442cfc1d02c3f2b85e36641a1
Vitālijs Korabeļņikovs <vitalijs.korabelnikovs@gmail.com>
2025-03-27 21:46:06 +0200

    Parcel

---
+++ src/calc.js
@@ -0,0 +1,10 @@
+function add(){
+    console.log("add")
+}
+
+function del(){
+    console.log("delete")
+}
+
+
+export {add,del}
--- src/index.js
+++ src/index.js
@@ -1,1 +1,34 @@
 import * as bootstrap from 'bootstrap';
+import MicroModal from 'micromodal';  // es6 module
+// import "./calc";
+
+import {add,del} from "./calc";
+
+
+
+MicroModal.init(
+    {
+        onShow: modal => console.info(`${modal.id} is shown`), // [1]
+        onClose: modal => console.info(`${modal.id} is hidden`), // [2]
+        openTrigger: 'data-custom-open', // [3]
+        closeTrigger: 'data-custom-close', // [4]
+        openClass: 'is-open', // [5]
+        disableScroll: true, // [6]
+        disableFocus: false, // [7]
+        awaitOpenAnimation: false, // [8]
+        awaitCloseAnimation: false, // [9]
+        debugMode: true // [10]
+      }
+);
+
+
+add();
+add();
+add();
+add();
+
+del();
+del();
+del();
+del();
+del();
